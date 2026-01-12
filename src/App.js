fcd6976d287b42b87103185d4944f68a0d95c6c5
Monishadhanasekar <monisha.dhanasekar@learnship.com>
2026-01-12 14:17:30 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const Grid = () => {
+  const [activeBoxes, setActiveBoxes] = useState([]);
+
+  const handleClick = (id) => {
+
+    setActiveBoxes((prev) => [...prev, id]);
+
+    setTimeout(() => {
+        setActiveBoxes((prev) => prev.filter((item) => item !==id))
+    }, 10000);
+  }
+
+  const boxes = [
+    { id: 1, area: "a" },
+    { id: 2, area: "b" },
+    { id: 3, area: "c" },
+    { id: 4, area: "d" },
+    { id: 5, area: "e" },
+    { id: 6, area: "f" },
+    { id: 7, area: "g" },
+    { id: 8, area: "h" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {boxes.map(({ id, area }) => (
+        <div
+          key={id}
+          className={`box ${area} ${activeBoxes.includes(id) ? "active" : ""}`}
+          onClick={() => handleClick(id)}
+        >        
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default Grid;
